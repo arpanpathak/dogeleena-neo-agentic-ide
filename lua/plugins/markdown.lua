b81f91eea5e2cfb24ddac398f6642f574f234caa
arpanpathak <arpan.pathak47@gmail.com>
2026-09-07 19:14:47 -0700

    fix: stop render-markdown crashing CodeCompanion chat buffers

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,7 +1,9 @@
+-- Keep render-markdown off CodeCompanion chat buffers: the extra Treesitter
+-- injection parsing there is what caused the recurring nil/redraw crashes.
 return {
   "MeanderingProgrammer/render-markdown.nvim",
   opts = {
-    file_types = { "markdown", "codecompanion" },
+    file_types = { "markdown" },
   },
   dependencies = { "nvim-treesitter/nvim-treesitter" },
 }
